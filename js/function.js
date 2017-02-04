7ad97a0bd2543339a12b75af017f3317b9ce1041
J <jakemckenzie19@gmail.com>
2017-02-03 20:15:38 -0800

    Add files via upload

--- js/function.js
+++ js/function.js
@@ -1,3 +1,14 @@
+// Fade in
+
+/*$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');},
+      3000);
+});
+*/
 // Scroll to element
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
